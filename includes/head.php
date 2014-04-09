ed6c5c0769b7a7e29cb172146a51995641bb479a
michaellaness <m@michaellaness.com>
2014-04-08 19:20:09 -0600

    meepp

--- includes/head.php
+++ includes/head.php
@@ -24,6 +24,18 @@
 
 	<link href='css/style.css' rel='stylesheet' type='text/css'>
 
+<script type="text/javascript">
+	var $buoop = {};
+	$buoop.ol = window.onload;
+	window.onload=function(){
+	 try {if ($buoop.ol) $buoop.ol();}catch (e) {}
+	 var e = document.createElement("script");
+	 e.setAttribute("type", "text/javascript");
+	 e.setAttribute("src", "//browser-update.org/update.js");
+	 document.body.appendChild(e);
+	}
+</script>
+
 <!--[if lt IE 9]>
 	<script src="js/html5.js"></script>
    <script>
